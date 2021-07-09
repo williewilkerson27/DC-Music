84f70503c2bd8a3b9ad610c2acdd16e617c3be1d
Wilky <williamwilkerson27@outlook.com>
2021-07-09 14:28:40 -0400

    merge issue

--- deezerApi.js
+++ deezerApi.js
@@ -1,25 +1,36 @@
-fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "30caeee35amsh028fb26bb6a6d1fp10bee7jsne480b16660b8",
-		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
-	}
-})
-    .then(response => {
-        console.log(response);
-        return response.json()
+// document.addEventListener('DOMContentLoaded', function () {
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=love", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "30caeee35amsh028fb26bb6a6d1fp10bee7jsne480b16660b8",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        }
     })
-    .then((data) => {
-        console.log(data)
-        return data.data
-    })
-    .then((songs) => {
-        let songsHtmlArray = songs.map((song) => {
-            return `${song.title}`
+        .then(response => {
+            // console.log(response);
+            return response.json()
         })
-        console.log(songsHtmlArray)
+        .then((data) => {
+            cards.innerHTML = renderMusic(data.data)
+        })
+        .catch(err => {
+            console.error(err);
+        });
 
-    })
-    .catch(err => {
-        console.error(err);
-    });
+        function renderMusic(musicArray) {
+            let songsHtmlArray = musicArray.map((result) => {
+                return `
+                <div class="card-img-top" style="width: 18rem;">
+                <img src="${result.artist.picture_medium}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${result.artist.name}</h5>
+                    <p id="artist-bio" class="card-text">${result.title}</p>
+                    <a href="#" class="btn btn-primary">Click</a>
+                </div>
+            </div>`
+            })
+            return songsHtmlArray.join('')
+        }
+        const cards = document.getElementById('artist-container')
+
+// })
